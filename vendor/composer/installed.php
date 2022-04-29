094ca28829b8e8b33ee1b61f2b926175d9b17a07
Nguyễn Chí Luận <Nguyen Luan>
2022-04-29 22:28:10 +0700

    new

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '6385ad9ecada143197d82afc0031ff8d80d194fe',
+        'name' => 'almasaeed2010/adminlte',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'almasaeed2010/adminlte' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '6385ad9ecada143197d82afc0031ff8d80d194fe',
+            'dev_requirement' => false,
+        ),
+    ),
+);
